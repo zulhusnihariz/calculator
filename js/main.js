f6d05ff9e7cc26b1a25f40661a138051dfbc7ecb
zulhusnihariz <zulhusni_97@yahoo.com>
2021-01-28 22:21:48 +0800

    added undo & ans button; more than 2 number can be given for the same operation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* -------------------------------------------------------------------------- */
-/*                        Declare element as variables                        */
+/*                        Declare element tag as variables                    */
 /* -------------------------------------------------------------------------- */
 
 const operation = document.querySelector("#operation");
@@ -7,7 +7,10 @@ const answer = document.querySelector("#answer");
 const equalSign = document.querySelector("#equal-sign");
 
 const clear = document.getElementById("CE");
+const undo = document.getElementById("undo");
+const ans = document.getElementById("ans");
 const equal = document.getElementById("=");
+const period = document.getElementById(".");
 const add = document.getElementById("+");
 const substract = document.getElementById("-");
 const multiply = document.getElementById("*");
@@ -24,9 +27,8 @@ const eight = document.getElementById("8");
 const nine = document.getElementById("9");
 
 /* -------------------------------------------------------------------------- */
-/*                   Clear screen and variables when pressed                  */
+/*                   Clear screen and when "CE" button is pressed             */
 /* -------------------------------------------------------------------------- */
-
 function clearScreen() {
 	operation.textContent = "";
 	answer.textContent = "";
@@ -34,13 +36,17 @@ function clearScreen() {
 }
 
 /* -------------------------------------------------------------------------- */
-/*                   Display whatever pressed on the screen                   */
+/*             Function to display number when a button is pressed            */
 /* -------------------------------------------------------------------------- */
 
 function displayNumber(anyNumber) {
 	operation.style.visibility == "hidden"
 		? (operation.style.visibility = "visible")
 		: (operation.style.visibility = "visible");
+
+	let undoThis;
+
+	undoThis = anyNumber;
 	if (operation != undefined) {
 		operation.textContent += anyNumber;
 	} else {
@@ -50,46 +56,93 @@ function displayNumber(anyNumber) {
 }
 
 /* -------------------------------------------------------------------------- */
-/*                        onclick for equal sign button                       */
+/*               Function to display "."; if exists, do nothing               */
+/* -------------------------------------------------------------------------- */
+
+function pressDecimal() {
+	currentDisplay = operation.textContent;
+	console.log(`firstindex ${currentDisplay.indexOf(".")}`);
+	console.log(` lastindex ${currentDisplay.lastIndexOf(".")}`);
+
+	if (currentDisplay.indexOf(".") != -1) {
+		console.log("exists!");
+		return;
+	} else {
+		displayNumber(".");
+	}
+}
+
+/* -------------------------------------------------------------------------- */
+/*                     Function for equal and undo button                     */
 /* -------------------------------------------------------------------------- */
 
 function pressEqual() {
 	if (operation.textContent != "") {
-		equalSign.textContent = "=";
 		calculate();
-		let currentAns = answer.textContent;
-		operation.textContent = currentAns;
+
+		operation.textContent = "";
 		operation.style.visibility = "hidden";
+
+		if (answer.textContent == "") {
+			return;
+		} else {
+			equalSign.textContent = "=";
+		}
 		// displayAnswer();
-	} else {
-		return;
 	}
+}
+
+function pressUndo() {
+	currentDisplay = operation.textContent;
+	console.log(currentDisplay);
+	currentDisplay = currentDisplay.slice(0, -1);
+	operation.textContent = currentDisplay;
 }
 
 /* -------------------------------------------------------------------------- */
 /*                                 Operations                                 */
 /* -------------------------------------------------------------------------- */
 
-function addition(firstNum, secondNum) {
-	return parseInt(firstNum) + parseInt(secondNum);
+function addition(...anyNumber) {
+	let sum = parseFloat(anyNumber[0]);
+	for (i = 1; i < anyNumber.length; i++) {
+		console.log(`This is typeof ${typeof parseFloat(anyNumber[i])}`);
+		sum += parseFloat(anyNumber[i]);
+		console.log(`This is sum ${parseFloat(sum)}`);
+	}
+	return sum;
 }
 
-function substraction(firstNum, secondNum) {
-	parseInt(firstNum);
-	parseInt(secondNum);
-	return parseInt(firstNum) - parseInt(secondNum);
+function substraction(...anyNumber) {
+	let sum = parseFloat(anyNumber[0]);
+	for (i = 1; i < anyNumber.length; i++) {
+		console.log(`This is sum ${typeof parseFloat(anyNumber[i])}`);
+		sum -= parseFloat(anyNumber[i]);
+	}
+	return sum;
 }
 
-function multiplication(firstNum, secondNum) {
-	parseInt(firstNum);
-	parseInt(secondNum);
-	return parseInt(firstNum) * parseInt(secondNum);
+function multiplication(...anyNumber) {
+	let sum = parseFloat(anyNumber[0]);
+	for (i = 1; i < anyNumber.length; i++) {
+		console.log(`This is sum ${anyNumber[i]}`);
+		sum *= parseFloat(anyNumber[i]);
+	}
+	return sum;
 }
 
-function division(firstNum, secondNum) {
-	parseInt(firstNum);
-	parseInt(secondNum);
-	return parseInt(firstNum) / parseInt(secondNum);
+function division(...anyNumber) {
+	let sum = parseFloat(anyNumber[0]);
+	for (i = 1; i < anyNumber.length; i++) {
+		console.log(`This is sum ${anyNumber[i]}`);
+		sum /= parseFloat(anyNumber[i]);
+	}
+
+	if (sum == Infinity) {
+		return "undefined";
+	} else {
+		return sum.toFixed(5);
+	}
 }
 
 /* -------------------------------------------------------------------------- */
@@ -101,60 +154,49 @@ function calculate() {
 	console.log(numberGiven);
 
 	if (numberGiven.includes("+")) {
+		// addition if plus sign exist
 		numberGiven = numberGiven.split("+");
 		console.log(numberGiven);
-		answer.textContent = addition(numberGiven[0], numberGiven[1]);
+		answer.textContent = addition(...numberGiven);
 	} else if (numberGiven.includes("-")) {
+		// substract if minus sign exist
 		numberGiven = numberGiven.split("-");
 		console.log(numberGiven);
-		answer.textContent = substraction(numberGiven[0], numberGiven[1]);
+		answer.textContent = substraction(...numberGiven);
 	} else if (numberGiven.includes("*")) {
+		// multiply if star sign exist
 		numberGiven = numberGiven.split("*");
 		console.log(numberGiven);
-		answer.textContent = multiplication(numberGiven[0], numberGiven[1]);
+		answer.textContent = multiplication(...numberGiven);
 	} else if (numberGiven.includes("/")) {
+		// division if divide sign exist
 		numberGiven = numberGiven.split("/");
 		console.log(numberGiven);
-		answer.textContent = division(numberGiven[0], numberGiven[1]);
+		answer.textContent = division(...numberGiven);
 	}
 }
-
-/*
-1. Grab current string when an operator is pressed
-2. Split into list, take out operator, obtained list of numbers
-
-[12972, "+", 9373]
-
-answer.textContent = addition(12972,9373);
-
-
-function addition (firstNum, secondNum){
-	return firstNum+secondNum
-}
-
-function substraction (firstNum, secondNum){
-	return firstNum-secondNum
-}
-
-function multiplication (firstNum, secondNum){
-	return firstNum*secondNum
-}
-
-function division (firstNum, secondNum){
-	return firstNum/secondNum
-}
-
-
-*/
-
-/* --------------------- onclick event for every button --------------------- */
+/* -------------------------------------------------------------------------- */
+/*                       Onclick event for every button                       */
+/* -------------------------------------------------------------------------- */
 
 clear.addEventListener("click", () => {
 	clearScreen();
 });
 
+undo.addEventListener("click", () => {
+	pressUndo();
+});
+
+ans.addEventListener("click", () => {
+	displayNumber(answer.textContent);
+});
+
 equal.addEventListener("click", () => {
 	pressEqual();
+});
+
+period.addEventListener("click", () => {
+	pressDecimal();
 });
 
 add.addEventListener("click", () => {
